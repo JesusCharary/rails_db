9caa76308ae88df6d88adcdb26aff142982d0d1b
Juan Parra <juandkpa@gmail.com>
2018-09-30 16:47:55 -0500

    theory: migrations - associations

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module DbExample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    #config.active_record.scheme_format :sql
   end
 end
